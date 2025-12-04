7892f2dee3e7f77e2b0cee7d884e048aa78c470d
Matthew Morrison <mmorrison@mcommgroup.com>
2025-12-03 19:27:36 -0600

    export_all.bat

--- bin/export_all.bat
+++ bin/export_all.bat
@@ -1,1 +1,1 @@
-"c:\Program Files\Blender Foundation\Blender 5.0\blender.exe" --addons --background --python "F:\Projects\ldraw\_python\ExportLDraw\export_all.py"
+"c:\Program Files\Blender Foundation\Blender 5.0\blender.exe" --addons --background --python "F:\Projects\ldraw\_python\ExportLDraw\bin\export_all.py"
